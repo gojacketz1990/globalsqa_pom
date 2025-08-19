01db71715d69dc72d55e49aeffbd0824119e833b
gojacketz1990 <gojacketz@gmail.com>
2025-08-19 09:04:44 -0400

    added locators for register and login

--- Selenium/GlobalsQAPOM/locators/angularjs_registration_login_locators.py
+++ Selenium/GlobalsQAPOM/locators/angularjs_registration_login_locators.py
@@ -1,4 +1,43 @@
 class AngularJSRegistrationLoginPageLocators():
-    first_name_search_locator = [
-        ("css","input[st-search='firstName']")
+    username_locator = [
+        ("id","username"),
+        ("name","username")
+    ]
+    password_locator = [
+        ("id","password"),
+        ("name","password")
+    ]
+
+    login_button_locator = [
+        ("css","button[type='submit']")
+    ]
+
+    register_link_locator = [
+        ("link_text","Register")
+    ]
+
+    #Register Fields:
+
+    register_first_name_locator = [
+        ("id","firstName"),
+        ("name","firstName")
+    ]
+
+    register_last_name_locator = [
+        ("id","lastName"),
+        ("name","lastName")
+    ]
+    register_username_locator = [
+        ("id","username"),
+        ("name","username")
+    ]
+    register_password_locator = [
+        ("id","password"),
+        ("name","password")
+    ]
+    register_button_locator = [
+        ("css","button[type='submit']")
+    ]
+    cancel_link_locator = [
+        ("link_text","Cancel")
     ]
--- Selenium/GlobalsQAPOM/pages/angularjs_registrationLoginPage.py
+++ Selenium/GlobalsQAPOM/pages/angularjs_registrationLoginPage.py
@@ -12,3 +12,34 @@ class AngularJSRegistrationLoginPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.angularJSRegistrationLoginPageLocators = AngularJSRegistrationLoginPageLocators()
+
+    def enter_username(self, username):
+        self.type_into_element(username, AngularJSRegistrationLoginPageLocators.username_locator)
+
+    def enter_password(self, password):
+        self.type_into_element(password, AngularJSRegistrationLoginPageLocators.password_locator)
+
+    def click_login_button(self):
+        self.element_click(AngularJSRegistrationLoginPageLocators.login_button_locator)
+
+
+    def click_register(self):
+        self.element_click(AngularJSRegistrationLoginPageLocators.register_link_locator)
+
+    def register_enter_first_name(self, firstname):
+        self.type_into_element(firstname, AngularJSRegistrationLoginPageLocators.register_first_name_locator)
+
+    def register_enter_last_name(self, lastname):
+        self.type_into_element(lastname, AngularJSRegistrationLoginPageLocators.register_last_name_locator)
+
+    def register_enter_username(self, username):
+        self.type_into_element(username, AngularJSRegistrationLoginPageLocators.register_username_locator)
+
+    def register_enter_password(self, password):
+        self.type_into_element(password, AngularJSRegistrationLoginPageLocators.register_password_locator)
+
+    def click_register_button(self):
+        self.element_click(AngularJSRegistrationLoginPageLocators.register_button_locator)
+
+    def click_cancel_registration_link(self):
+        self.element_click(AngularJSRegistrationLoginPageLocators.cancel_link_locator)
